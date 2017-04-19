a62e2418ceedad5380afb7b3aa17b03504c94eb8
Dnyaneshwar Dalvi <dnyaneshwar.dalvi@interactive.in>
2017-04-19 16:15:40 +0530

    Create project

---
+++ androidlib/src/main/java/libs/in/androidlib/AppDemo.java
@@ -0,0 +1,17 @@
+package libs.in.androidlib;
+
+import android.util.Log;
+
+/**
+ * Created by apple on 19/04/17.
+ */
+
+public class AppDemo
+{
+    public static final String TAG = "AWESOME_APP";
+
+    public static void d(String msg)
+    {
+        Log.d(TAG, msg);
+    }
+}

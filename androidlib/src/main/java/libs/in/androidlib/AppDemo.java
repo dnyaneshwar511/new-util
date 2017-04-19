c15369f8463016e30029dc20e929e7377efae693
Dnyaneshwar Dalvi <dnyaneshwar.dalvi@interactive.in>
2017-04-19 18:07:35 +0530

    new print function

--- androidlib/src/main/java/libs/in/androidlib/AppDemo.java
+++ androidlib/src/main/java/libs/in/androidlib/AppDemo.java
@@ -1,6 +1,8 @@
 package libs.in.androidlib;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by apple on 19/04/17.
@@ -13,5 +15,10 @@ public class AppDemo
     public static void d(String msg)
     {
         Log.d(TAG, msg);
+    }
+
+    public static void printText(Context context, String str)
+    {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
     }
 }
